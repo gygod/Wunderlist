26b4914feffd17cfc44e12b136c15fb87bbb0b36
Robby <ta7382@gmail.com>
2018-02-26 04:21:29 +0800

    Update index.rs

--- index.rs
+++ index.rs
@@ -5,3 +5,15 @@ fn main() {
     println!("Let’s do your best to your dream with GYGOD and make your dream come true!")
     
 }
+
+/*
+ *    Please wait, loading source from GitHub ...
+ *
+ *
+ *                       /\_/\
+ *                      / - - \
+ *                     <_  X  _>  /\_/\
+ *                     /       \ <_o_o_>
+ *                    <_)_U_U_(_> 
+ *
+ */
